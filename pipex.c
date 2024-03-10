f4890e0ca9a371215ea71c9e1ff8b2885cf88929
Sumi Seo <sumseo@bess-f3r1s13.clusters.42paris.fr>
2024-03-10 19:02:15 +0100

    first-commit

---
+++ pipex.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/10 18:31:06 by sumseo            #+#    #+#             */
+/*   Updated: 2024/03/10 18:35:37 by sumseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	pipe(int ends[2])
+{
+	// if it returns 0 : Successfully piped
+	// if it returns -1 : Failure
+	// In this case I have to handle the errors
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+    
+	}
+    
+    
+	else
+		return ;
+}
